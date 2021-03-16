fb38b9a8e9507bcf7a688d9814cd563d09a75562
fullinthestudio <77401660+fullinthestudio@users.noreply.github.com>
2021-03-17 00:28:24 +0300

    Update Addusers.py

--- Addusers.py
+++ Addusers.py
@@ -14,7 +14,7 @@ class addmembersMod(loader.Module):
         if len(event.text.split()) == 2:
             idschannelgroup = event.text.split(" ", maxsplit=1)[1]
             user = [i async for i in event.client.iter_participants(event.to_id.channel_id)]
-            await event.edit(f"<b>{len(user)} пользователей будет приглашено из чата {event.to_id.channel_id} в чат/канал {idschannelgroup}</b>")
+            
             
             for u in user:
                 try:
